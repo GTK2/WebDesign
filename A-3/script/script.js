3cc66b4a29911332cc4c13720f13747813250ce6
GTK <62057850+GTK2@users.noreply.github.com>
2021-04-29 16:54:55 +0900

    A-3 complete

--- A-3/script/script.js
+++ A-3/script/script.js
@@ -8,4 +8,25 @@ $(document).ready(function(){
     setInterval(function(){
         $(".imglist li:first").fadeOut(1000).next().fadeIn(1000).end().appendTo(".imglist");
     }, 3000);
+
+    var tabBtn = $(".tab-btn > ul > li");
+    var tabCont = $(".tab-cont > div");
+
+    tabCont.hide().eq(0).show();
+    tabBtn.click(function(e){
+        e.preventDefault();
+        var target = $(this);
+        var index = target.index();
+        tabBtn.removeClass("on");
+        target.addClass("on");
+        tabCont.hide();
+        tabCont.eq(index).show();
+    });
+
+    $(".notice ul li").eq(0).click(function(){
+        $("#modal").show();
+    });
+    $(".popup input").click(function(){
+        $("#modal").hide();
+    });
 });
