c02d2339d3ce5564356a0b7f586984a26181f71b
GTK <62057850+GTK2@users.noreply.github.com>
2021-05-24 16:02:57 +0900

    이미지 슬라이드 주석처리

--- 이미지슬라이드 소스/script/script.js
+++ 이미지슬라이드 소스/script/script.js
@@ -1,9 +1,18 @@
 $(document).ready(function(){
+    // 필요한 변수 선언
+    // index : 다음에 보여질 이미지. 즉 흘러 들어올 이미지의 인덱스
+    // prev : 현재 화면 보이는 이미지. 즉 흘러 나갈 이미지의 인덱스
+    // $imgList : .imglist의 li 요소들을 배열로 저장
+    // totalNum : $imgList의 크기를 저장
     var index = 0, prev, $imgList = $(".imglist > li"), totalNum = $imgList.length;
 
+    // 15 line : setInterval : 정해진 시간마다 계속 실행하는 함수
+    // 16~17 line : 현재 보여지는 이미지와 다음에 보여질 이미지 인덱스 값 설정
+    // 18 line : 다음에 보여질 이미지의 인덱스가 전체 이미지 개수 이상이면 인덱스를 0으로 설정(1, 2, 3번 이미지가 모두 보여진 후 다시 1번부터 시작)
+    // 19 line : imgList의 index 번째 요소의 css 값을 설정(화면 밖 위치로 준비시킴)
+    // 20 line : imgList의 index 번째 요소를 지정된 css 값 위치로 슬라이드 시킴(화면 밖에서 화면 안으로 슬라이드 인)
+    // 21 line : imgList의 prev 번째 요소를 지정된 css 값 위치로 슬라이드 시킴(화면 안에서 화면 밖으로 슬라이드 아웃)
     setInterval(function(){
-        // prev = 현재 화면 보이는 이미지. 즉 흘러 나갈 이미지
-        // index = 다음에 보여질 이미지. 즉 흘러 들어올 이미지
        prev = index; 
        index = index + 1;
        if(index >= totalNum) index = 0;
--- 페이드인 소스/script/script.js
+++ 페이드인 소스/script/script.js
@@ -1,4 +1,12 @@
 $(document).ready(function(){
+    // 이미지 슬라이드(페이드 인아웃)
+    // 8 line : 전체 이미지를 가리고
+    // 9 line : 첫번째 이미지만 보이게 함
+    // 11 line : $(.img-list li:first-child).fadeOut() : img-list의 첫번째 li 요소를 페이드 아웃시킴
+    // .next().fadeIn() : 현재 선택된(첫번쨰 li요소)의 다음 요소를 페이드 인시킴
+    // .end().appanedTo(".img-list") : 현재 선택된(next에서 선택된 두번째 li 요소)의 이전 선택 요소(img-list의 첫번째 li요소)를 잘라서 img-list의 맨 뒤에 붙여넣음. (1번 이미지가 리스트의 맨 뒤로가서 전체 이미지의 순서가 2 - 3 - 1로 바뀜. 그래서 다음 실행시에는 2번 이미지가 img-list의 첫번째 li 요소가 됨.)
+    $(".imglist li").hide(); 
+    $(".imglist li:first-child").show();    
     setInterval(function(){
         $(".imglist li:first-child").fadeOut(1500).next().fadeIn(1500).end().appendTo(".imglist");
     }, 3000);
