850f876b4766c3aaf12d99f52cc8d9d0e6a3bb41
kai2rock <51189053+kai2rock@users.noreply.github.com>
2021-06-02 11:45:44 +0900

    B-2 완성

--- B-2/script/script.js
+++ B-2/script/script.js
@@ -9,6 +9,18 @@ $(document).ready(function(){
 
     var index = 0, prev, imgList = $(".imglist li"), size = imgList.length;
     setInterval(function(){
-        
+        prev = index;
+        index++;
+        if(index >= size) index = 0;
+        imgList.eq(index).css({top : 300 + 'px'});
+        imgList.eq(index).animate({top : 0}, 1000);
+        imgList.eq(prev).animate({top : -300 + 'px'}, 1000);
     }, 3000);
+
+    $(".notice li:first-child").click(function(){
+        $("#modal").show();
+    });
+    $(".popup input").click(function(){
+        $("#modal").hide();
+    });
 });
